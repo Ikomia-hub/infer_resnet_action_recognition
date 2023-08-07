f611982c735b4bddec52cfa6c2e322b31c5b8957
ambroise <ambroise.berthe@ikomia.com>
2023-08-07 15:28:43 +0200

    Fix test file

--- infer_resnet_action_recognition_test.py
+++ infer_resnet_action_recognition_test.py
@@ -1,7 +1,6 @@
 import logging
 import cv2
 from ikomia.utils.tests import run_for_test
-from ikomia.core import task
 import os
 
 logger = logging.getLogger(__name__)
@@ -13,7 +12,7 @@ def test(t, data_dict):
     img = cv2.imread(data_dict["images"]["detection"]["coco"])
     input_img_0 = t.get_input(0)
     input_img_0.set_image(img)
-    params = t.get_param_object()
+    params = t.get_parameters()
     params["model_path"] = os.path.join(os.path.dirname(os.path.abspath(__file__)), "models", "resnext-101-kinetics.onnx")
     t.set_parameters(params)
     return run_for_test(t)
