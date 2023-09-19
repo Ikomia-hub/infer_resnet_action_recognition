5b264be003c78a3f48657aac552929867bfa7944
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2023-09-19 10:16:02 +0200

    Feat: update output management (use of CGraphicsOutput)

--- infer_resnet_action_recognition_process.py
+++ infer_resnet_action_recognition_process.py
@@ -39,12 +39,13 @@ class ResNetActionRecognitionParam(core.CWorkflowTaskParam):
     def get_values(self):
         # Send parameters values to Ikomia application
         # Create the specific dict structure (string container)
-        param_map = {}
-        param_map["model_name"] = str(self.model_name)
-        param_map["rolling"] = str(self.rolling)
-        param_map["sample_duration"] = str(self.sample_duration)
-        param_map["backend"] = str(self.backend)
-        param_map["target"] = str(self.target)
+        param_map = {
+            "model_name": str(self.model_name),
+            "rolling": str(self.rolling),
+            "sample_duration": str(self.sample_duration),
+            "backend": str(self.backend),
+            "target": str(self.target)
+        }
         return param_map
 
 
@@ -56,6 +57,7 @@ class ResNetActionRecognition(dataprocess.CVideoTask):
 
     def __init__(self, name, param):
         dataprocess.CVideoTask.__init__(self, name)
+        self.add_output(dataprocess.CGraphicsOutput())
         self.net = None
         self.last_label = ""
         self.class_names = []
@@ -80,12 +82,12 @@ class ResNetActionRecognition(dataprocess.CVideoTask):
         # This is handled by the main progress bar of Ikomia application
         return 1
 
-    def globalInputChanged(self, new_sequence):
+    def global_input_changed(self, new_sequence):
         if new_sequence:
             self.frames = None
             self.last_label = ""
 
-    def notifyVideoStart(self, frame_count):
+    def notify_video_start(self, frame_count):
         self.frames = None
         self.last_label = ""
 
@@ -136,7 +138,7 @@ class ResNetActionRecognition(dataprocess.CVideoTask):
             outputs = self.net.forward()
             class_index = np.argmax(outputs)
 
-            if class_index >= 0 and class_index < len(self.class_names):
+            if 0 <= class_index < len(self.class_names):
                 self.last_label = self.class_names[class_index]
             else:
                 self.last_label = ""
@@ -146,15 +148,9 @@ class ResNetActionRecognition(dataprocess.CVideoTask):
 
         # Get output :
         self.forward_input_image(0, 0)
-        output_img = self.get_output(0)
-        dst_image = output_img.get_image()
-
-        # draw the predicted activity on the frame
-        if self.last_label != "":
-            labelSize, baseLine = cv2.getTextSize(self.last_label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
-            cv2.rectangle(dst_image, (0, 0), (labelSize[0] + 20, labelSize[1] + 10), (32, 32, 32), cv2.FILLED)
-            cv2.putText(dst_image, self.last_label, (10, int((labelSize[1]+10) / 2) + baseLine), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
-            output_img.set_image(dst_image)
+        graphics_out = self.get_output(1)
+        graphics_out.clear_data()
+        graphics_out.add_text(self.last_label, 10, 10)
 
         # Step progress bar:
         self.emit_step_progress()
@@ -174,24 +170,9 @@ class ResNetActionRecognitionFactory(dataprocess.CTaskFactory):
         # Set process information as string here
         self.info.name = "infer_resnet_action_recognition"
         self.info.short_description = "Human action recognition with spatio-temporal 3D CNNs."
-        self.info.description = "The purpose of this study is to determine whether current video datasets have sufficient data " \
-                                "for training very deep convolutional neural networks (CNNs) with spatio-temporalthree-dimensional " \
-                                "(3D) kernels. Recently, the performance levels of 3D CNNs in the field of action recognition have " \
-                                "improved significantly. However, to date, conventional research has only explored relatively shallow  " \
-                                "3D architectures. We examine the architectures of various 3D CNNs from relatively shallow to very deep " \
-                                "ones on current video datasets. Based on the results of those experiments, the following conclusions " \
-                                "could be obtained: (i) ResNet-18 training resulted in significant overfitting for UCF-101, HMDB-51, " \
-                                "and ActivityNet but not for Kinetics. (ii) The Kinetics dataset has sufficient data for training of " \
-                                "deep 3D CNNs, and enables training of up to 152 ResNets layers, interestingly similar to 2D ResNets on ImageNet. " \
-                                "ResNeXt-101 achieved 78.4% average accuracy on the Kinetics test set. (iii) Kinetics pre-trained simple 3D " \
-                                "architectures outperforms complex 2D architectures, and the pretrained ResNeXt-101 achieved 94.5% and 70.2% " \
-                                "on UCF-101 and HMDB-51, respectively. The use of 2D CNNs trained on ImageNet has produced significant progress " \
-                                "in various tasks in image. We believe that using deep 3D CNNs together with Kinetics will retrace the successful " \
-                                "history of 2D CNNs and ImageNet, and stimulate advances in computer vision for videos. The codes and pretrained " \
-                                "models used in this study are publicly available."
         # relative path -> as displayed in Ikomia application process tree
         self.info.path = "Plugins/Python/Classification"
-        self.info.version = "1.3.0"
+        self.info.version = "1.4.0"
         self.info.icon_path = "icon/icon.png"
         self.info.authors = "Kensho Hara, Hirokatsu Kataoka, Yutaka Satoh"
         self.info.article = "Can Spatiotemporal 3D CNNs Retrace the History of 2D CNNs and ImageNet?"
